c5465d09984a87fe30faebe338f690b5d7069757
James Acklin <james.acklin@gmail.com>
2018-12-01 11:34:28 -0500

    beginning to frame out post atom

---
+++ src/components/PostAtom.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+  <div class="post-container">
+    <div class="post">
+
+      <div class="post-title-block">
+        <h4 class="post-title">
+          <span class="post-title-callout">
+            Post Title Callout
+          </span>
+          <span class="post-title-wrap">
+            Post Title
+          </span>
+        </h4>
+        <div class="post-video-watch">
+          > Watch Video
+        </div>
+      </div>
+
+      <div class="post-author">
+        Post Author
+        <span class="post-date">
+          &nbsp;—&nbsp;
+          <span>Post Date</span>
+        </span>
+      </div>
+
+      <div class="post-excerpt">
+        <p>Post Excerpt</p>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostAtom",
+  props: {},
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style lang="css">
+.post-title-block {
+
+}
+
+.post-title {
+
+}
+
+.post-title-callout {
+  display: block;
+}
+</style>
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,25 @@
 /* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
 
-import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import HelloWorld from '../src/components/HelloWorld';
+import HelloWorld from "../src/components/HelloWorld";
+import PostAtom from "../src/components/PostAtom";
 
-storiesOf('Hello World', module)
-  .add('Default', () => ({
+storiesOf("Hello World", module)
+  .add("Default", () => ({
     components: { HelloWorld },
     template: `<HelloWorld />`
   }))
-  .add('Custom Message', () => ({
+  .add("Custom Message", () => ({
     components: { HelloWorld },
     template: `<HelloWorld msg="Welcome to Your Vue.js App"/>`
   }));
+
+storiesOf("Post", module).add("Default", () => ({
+  components: { PostAtom },
+  template: `<PostAtom />`
+}));
 
 /* eslint-enable react/react-in-jsx-scope */
