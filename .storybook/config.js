f2b6c40176bc9360017373190f02636a4c63887e
James Acklin <james.acklin@gmail.com>
2018-12-03 17:24:16 -0500

    center-align decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,13 @@
-import { configure } from '@storybook/vue';
+import { configure, addDecorator } from "@storybook/vue";
+
+// Decorator to center-align all stories
+addDecorator(() => ({
+  template:
+    '<div style="padding: 3rem; display: flex; flex-direction: column; align-items: center" ><story/></div>'
+}));
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context("../stories", true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
