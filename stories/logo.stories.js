29dbae3ef886e9f4144e78418315efe87d4d62e4
James Acklin <james.acklin@gmail.com>
2018-12-03 19:58:42 -0500

    horizontal and vertical logos

---
+++ stories/logo.stories.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
+
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+import {
+  withKnobs,
+  text,
+  boolean,
+  number,
+  date,
+  select
+} from "@storybook/addon-knobs";
+import { checkA11y } from "@storybook/addon-a11y";
+
+import Logo from "../src/components/Logo.vue";
+
+storiesOf("Logo", module)
+  .addDecorator(withKnobs)
+  .add("Vertical (Default)", () => ({
+    components: { Logo },
+    template: `<Logo orientation="vertical" />`
+  }))
+  .add("Horizontal", () => ({
+    components: { Logo },
+    template: `<Logo orientation="horizontal" />`
+  }));
+
+/* eslint-enable react/react-in-jsx-scope */
