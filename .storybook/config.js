d779c02f938702a441be1412cffa6a6611ed2f92
James Acklin <james.acklin@gmail.com>
2018-12-05 20:35:27 -0500

    ripping out decorator because it's causing trouble with some things

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,5 @@
 import { configure, addDecorator } from "@storybook/vue";
 
-// Decorator to center-align all stories
-addDecorator(() => ({
-  template:
-    '<div style="padding: 3rem; display: flex; flex-direction: column; align-items: center" ><story/></div>'
-}));
-
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /.stories.js$/);
 function loadStories() {
