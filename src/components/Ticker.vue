7ade5d4b691086a9a0f06f5e2aa66421a3121ff1
James Acklin <james.acklin@gmail.com>
2018-12-05 21:09:44 -0500

    news ticker, pauses on hover, handles links

---
+++ src/components/Ticker.vue
@@ -0,0 +1,82 @@
+<template lang="html">
+  <div class="ticker" @mouseover="pauseTicker()" @mouseout="resumeTicker()">
+    <marquee-text :paused="this.paused">
+      <span class="ticker-badge">Dirt Rag Newswire</span>
+      <span
+        class="ticker-story"
+        v-for="story in tickerStories">
+        <a :href="story.link">{{ story.title }}</a>
+      </span>
+    </marquee-text>
+  </div>
+</template>
+
+<script>
+import MarqueeText from "vue-marquee-text-component";
+
+export default {
+  name: "Ticker",
+  components: {
+    MarqueeText
+  },
+  data: function() {
+    return {
+      paused: false
+    };
+  },
+  props: {
+    tickerStories: Array
+  },
+  methods: {
+    pauseTicker: function() {
+      this.paused = true;
+    },
+    resumeTicker: function() {
+      this.paused = false;
+    }
+  }
+};
+</script>
+
+<style lang="css">
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono:300');
+
+.ticker {
+  background: black;
+  padding: 0.25rem 0;
+}
+
+.ticker-story {
+  font-family: "Roboto Mono", monospace;
+  font-weight: 300;
+  color: white;
+  display: inline-block;
+  text-transform: uppercase;
+}
+
+.ticker-story a {
+  color: white;
+  text-decoration: none;
+}
+
+.ticker-story a:hover {
+  text-decoration: underline;
+}
+
+.ticker-story::after  {
+  content: "•";
+  margin: 0 1rem;
+}
+
+.ticker-badge {
+  font-family: "Roboto Mono", monospace;
+  text-transform: uppercase;
+  font-weight: 300;
+  color: white;
+  background: red;
+  display: inline-block;
+  padding: 0 0.25rem;
+  margin-right: 1rem;
+}
+
+</style>
---
+++ stories/ticker.stories.js
@@ -0,0 +1,45 @@
+/* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
+
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+import { withKnobs, array } from "@storybook/addon-knobs";
+import { checkA11y } from "@storybook/addon-a11y";
+
+import Ticker from "../src/components/Ticker";
+
+storiesOf("Ticker", module)
+  .addDecorator(withKnobs)
+  .add("Ticker", () => ({
+    components: { Ticker },
+    props: {
+      tickerStories: {
+        type: Array,
+        default: array("Ticker Stories", [
+          {
+            title: "Pivot to return as 2019 DirtFest Sponsor",
+            link: "https://www.google.com/"
+          },
+          {
+            title: "Snow Bike Festival is Back",
+            link: "https://www.google.com/"
+          },
+          {
+            title: "Canyon releases the new Neuron",
+            link: "https://www.google.com"
+          },
+          {
+            title: "Hark! Dirt Rag Issue 208 is on its way!",
+            link: "https://www.google.com"
+          },
+          {
+            title: "Remember to shred the vote November 6th",
+            link: "https://www.google.com"
+          }
+        ])
+      }
+    },
+    template: `<Ticker :tickerStories="tickerStories" />`
+  }));
+
+/* eslint-enable react/react-in-jsx-scope */
