1c35d91849bfacdee4eb48437499aeb4f753cc64
James Acklin <james.acklin@gmail.com>
2019-01-10 10:37:27 -0500

    use css grid for article page and header, outside player floats atop

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -6,7 +6,8 @@
           <Logo style="cursor: pointer;" orientation="horizontal" />
         </nuxt-link>
       </div>
-      <OutsideFeed />
+      <div class="header-sidebar"><OutsideFeed /></div>
+
       <Ticker ref="ticker" :tickerStories="tickerStories" />
     </header>
   </div>
@@ -36,8 +37,9 @@ export default {
   },
   methods: {
     scrollHeader() {
+      // FIXME: this
       const scrollY = window.scrollY
-      const tickerHeight = 30
+      const tickerHeight = 300
       if (scrollY >= tickerHeight) {
         return true
       } else {
@@ -58,17 +60,40 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
-
-header[role='banner']{
+<style lang="scss" scoped>
+header[role='banner'] {
   top: 0;
   width: 100%;
   background: #000000;
-  transition: all 0.2s ease;
+  /* transition: background-color 0.2s ease, height 0.2s ease; */
+  display: grid;
+  grid-template-columns: [full-start] minmax(1em, 1fr) [main-start] minmax(
+      0,
+      45em
+    ) [main-end] 300px minmax(1em, 1fr) [full-end];
+  grid-auto-rows: auto;
+  grid-column-gap: 1em;
+  grid-row-gap: 0;
+  @media (min-width: 1000px) {
+    grid-template-columns: [full-start] minmax(1em, 1fr) [main-start] minmax(
+        0,
+        45em
+      ) [main-end sidebar-start] 300px [sidebar-end] minmax(1em, 1fr) [full-end];
+    grid-column-gap: 2em;
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns:
+      [full-start] minmax(1em, 1fr)
+      [main-start] minmax(
+        0,
+        62.5em
+      ) [main-end sidebar-start] 300px [sidebar-end]
+      minmax(1em, 1fr) [full-end];
+  }
 }
 
 header a {
-  font-family: "Roboto Mono";
+  font-family: 'Roboto Mono';
   color: white;
 }
 
@@ -83,51 +108,58 @@ header a.nuxt-link-exact-active {
 
 .block {
   position: relative;
-  height: 77px;
+  height: 200px;
 }
 
 header.sticky {
   position: fixed;
   z-index: 2;
+  background: transparent;
 }
 
 .logo-wrapper {
+  grid-column: main;
   width: 200px;
-  padding: 1rem 0 1rem 2%;
-  transition: all 0.2s ease;
-  /* margin: 0 auto; */
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+  /* transform: translateX(-0.5em) */
 }
 
 header.sticky .logo-wrapper {
-  width: 125px;
-  padding: 0.25rem 0 0.25rem 2%;
+  background: #000;
+  width: 150px;
 }
 
-@media (max-width: 812px){
+header .header-sidebar {
+  grid-column: sidebar;
+}
+
+@media (max-width: 812px) {
   header .outside-player {
     display: none;
   }
 }
 
-header .outside-player {
-  position: absolute;
-  z-index: 2;
-  top: 1rem;
-  right: 2%;
-  width: 16em;
-  transition: all 0.2s ease;
-}
-
-header .outside-player:not(:empty){
-  outline: 1em solid black;
-}
-
-header.sticky .outside-player:not(:empty){
-  outline: 0.5em solid black;
-  transform: translateX(0.25em)
+header .outside-player:not(:empty) {
 }
 
 header.sticky .outside-player {
-  width: 12em;
+  position: absolute;
+  width: 300px;
+}
+
+header.sticky .outside-player:not(:empty) {
+}
+
+header .ticker {
+  grid-column: full;
+  grid-row: 1;
+}
+
+header.sticky .ticker {
+  visibility: hidden;
+  height: 0px;
+  grid-row: 1;
 }
 </style>
