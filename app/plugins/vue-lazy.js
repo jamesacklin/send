98612bd2633836779ec40168157f20d543a7e71f
James Acklin <james.acklin@gmail.com>
2019-01-16 15:56:56 -0500

    Break things out into plugins

--- app/plugins/vue-lazy.js
+++ app/plugins/vue-lazy.js
@@ -1,22 +1,9 @@
 import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
 
-import SocialSharing from 'vue-social-sharing'
-
-Vue.use(SocialSharing);
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFacebookSquare, faTwitter, faReddit } from '@fortawesome/free-brands-svg-icons'
-import { faLink } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-
 Vue.use(VueLazyload, {
   preLoad: 1.6,
   throttleWait: 600,
   error: '/og-card.png',
   loading: '/bike-animated-loop.svg'
 })
-
-library.add(faFacebookSquare, faTwitter, faReddit, faLink)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
