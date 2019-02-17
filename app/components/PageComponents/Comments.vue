f71fb39c969270ced56ca58578d17bb92ef870a8
James Acklin <james.acklin@gmail.com>
2019-02-17 09:49:52 -0500

    Disqus plugin, may need work with props

---
+++ app/components/PageComponents/Comments.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="comments">
+     <vue-disqus shortname="dirtragmag" :identifier="page_id" :url="page_url"></vue-disqus>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comments',
+  computed: {
+    page_id() {
+      return this.$route.fullPath
+    },
+    page_url() {
+      return `https://www.dirtragmag.com${this.$route.fullPath}`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
