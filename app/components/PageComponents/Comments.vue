f51bd82b5a8f5249ae2d27de4c8b9bf7c9871df2
James Acklin <james.acklin@gmail.com>
2019-02-17 14:44:51 -0500

    Configure Disqus plugin properly

--- app/components/PageComponents/Comments.vue
+++ app/components/PageComponents/Comments.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="comments">
-     <vue-disqus shortname="dirtragmag" :identifier="page_id" :url="page_url"></vue-disqus>
+    <vue-disqus 
+      shortname="dirt-rag-mag" 
+      :identifier="page_url"
+      :url="page_url"
+    />
   </div>
 </template>
 
@@ -8,16 +12,15 @@
 export default {
   name: 'Comments',
   computed: {
-    page_id() {
-      return this.$route.fullPath
-    },
     page_url() {
-      return `https://www.dirtragmag.com${this.$route.fullPath}`
+      // This isn't the *acutal* page URL, but it lines up with the permalink scheme
+      // from the old site (which doesn't have an /articles/%postname% prefix).
+      // Whatever, the Disqus comments seem to work, so let's leave it at that.
+      return `https://www.dirtragmag.com/${this.$route.params.slug}`
     }
   }
 }
 </script>
 
 <style>
-
 </style>
