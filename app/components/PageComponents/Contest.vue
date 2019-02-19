c569745a1eb35abbaba36700d5c133b7e99395f3
James Acklin <james.acklin@gmail.com>
2019-02-19 07:33:53 -0500

    Comments

--- app/components/PageComponents/Contest.vue
+++ app/components/PageComponents/Contest.vue
@@ -5,73 +5,77 @@
 </template>
 
 <script>
+// Import jQuery for DOM manipulation stuff (oof)
 import jQuery from 'jquery'
 let $ = jQuery
 
 export default {
-
   props: [
     'platform',
     'embedCode'
   ],
   mounted () {
-    var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
-    var match;
+    // TODO: make this work
+    // if (this.platform === 'Surveymonkey'){
+    //    // TODO: do all that other crap
+    // } else if (this.platform === 'Rafflecopter'){
+    //   let contestScript = document.createElement('script')
+    //   // Sets some attributes
+    //   contestScript.setAttribute(
+    //     'src',
+    //     'https://widget-prime.rafflecopter.com/launch.js'
+    //   )
+    //   contestScript.setAttribute('id', 'contest-script')
+    //   // Append the script to the head
+    //   document.head.appendChild(contestScript)
+    // }
+    //
+    // TODO: Put this in the "surveymonkey" conditional
+    // Test for script contents in the embedCode prop
+    var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm
+    var match
     while (match = re.exec(this.embedCode)) {
-      let monkeyScript = document.createElement('script')
-      // Sets some attributes      
-      monkeyScript.setAttribute('id', 'contest-script')
-      monkeyScript.innerHTML = match[1]
+      // If we find a match in the embed code...
+      // Create a script element
+      let contestScript = document.createElement('script')
+      // Set an ID so we can destroy it later
+      contestScript.setAttribute('id', 'contest-script')
+      // match[1] is the matched string itself, sans script tags
+      // Set the innerHTML to the matched script string
+      contestScript.innerHTML = match[1]
       // Appends the script to the head
-      console.log('adding script to head')
-      document.head.appendChild(monkeyScript)
-      var targetNode = document.body
-      var observerOptions = {
+      document.head.appendChild(contestScript)
+      // Now, set up a MutationObserver to watch for the widget to mount
+      var observer = new MutationObserver(callback);
+      // Watch the body for childList, attribute, and subtree changes
+      observer.observe(document.body, {
         childList: true,
         attributes: true,
         subtree: true
-      }
-      var observer = new MutationObserver(callback);
-
-      observer.observe(targetNode, observerOptions);
-
-      function callback(mutationList, observer){ 
+      })
+      // Called when a mutation is observed
+      function callback(mutationList, observer){
+        // Examine each mutation 
         mutationList.forEach(function(mutation){
-          console.log(mutation)
+          // If the mutation target className is this string, the Surveymonkey widget has mounted,
+          // probably not at all where we want the widget to actually display
           if (mutation.target.className === 'smcx-widget smcx-embed smcx-show smcx-widget-dark'){
-            $('.smcx-widget').appendTo('#target');
+            // Rip the widget out and put it where we want to on the page
+            $('.smcx-widget').appendTo('#target')
           }
         })
       }
     }
   },
   beforeDestroy() {
-    let contestScript = document.getElementById('contest-script')
-    console.log('removing contest script')
-    $("#contest-script").remove();
-    $('#__smcx__').remove();
-    $('iframe').remove();
-    $('#smcx-sdk').remove();
-  },
-  beforeMount() {
-
-
-
-
-    // if (this.platform === 'Surveymonkey'){
-    
-    // } else if (this.platform === 'Rafflecopter'){
-    //   // TODO: Put this behind some conditional
-    //   let raffleScript = document.createElement('script')
-    //   // Sets some attributes
-    //   raffleScript.setAttribute(
-    //     'src',
-    //     'https://widget-prime.rafflecopter.com/launch.js'
-    //   )
-    //   raffleScript.setAttribute('id', 'contest-script')
-    //   // Appends the script to the head
-    //   document.head.appendChild(raffleScript)
-    // }
+    // Find the contest-script in the head and destroy it
+    $("#contest-script").remove()
+    // Destroy some other Surveymonkey hooks
+    $('#__smcx__').remove()
+    // Destroy the Surveymonkey SDK script, which is also loaded for some reason
+    $('#smcx-sdk').remove()
+    // Remove all iframes while we're at it
+    $('iframe').remove()
   }
 }
 </script>
