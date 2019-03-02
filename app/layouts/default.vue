724a1f0ef99a3c0edc2ac7350363ba34de3b9101
James Acklin <james.acklin@gmail.com>
2019-03-02 16:56:13 -0500

    Red SVG filter on postatom hover

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="app-view">
-    <Header />
-    <NavDrawer />
-    <nuxt />
-    <Footer />
-    <Pagination />
+    <Header/>
+    <NavDrawer/>
+    <nuxt/>
+    <Footer/>
+    <Pagination/>
+    <svg id="filters" xmlns="http://www.w3.org/2000/svg" height="0">
+      <filter id="red">
+        <feColorMatrix type="matrix" values="0.1 2 0 0.7 0.1 0 1 0 0 0 0 1 0 0 0 0 1 0 1 0 "></feColorMatrix>
+      </filter>
+    </svg>
   </div>
 </template>
 
@@ -27,7 +32,7 @@ export default {
 <style lang="css" scoped>
 /* FIXME: Figure out some way to pad the bottom of the page based on the pagination height */
 .app-view {
-  background-color: #F5F3EF;
+  background-color: #f5f3ef;
   padding-bottom: 40px;
 }
 </style>
