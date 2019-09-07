d2dac83ebeb7000530ef76a54d4dc23d5e0fb7a5
James Acklin <james.acklin@gmail.com>
2019-09-07 10:51:26 -0400

    adding dirtfest external link, still figuring out styles

--- app/components/Navigation/NavItem.vue
+++ app/components/Navigation/NavItem.vue
@@ -1,14 +1,44 @@
 <template lang="html">
   <div>
-    <nuxt-link v-if="!isUrl(link)" :to="link" tag="a" class="nav-item"> {{ text }} </nuxt-link>
-    <a v-if="isUrl(link)" :href="link" class="nav-item nav-item-external">{{ text }}</a>
+    <nuxt-link 
+      v-if="!isUrl(link)" 
+      :to="link" 
+      tag="a" 
+      class="nav-item"
+    >
+      {{ text }}
+    </nuxt-link>
+    <a 
+      v-if="isUrl(link)" 
+      :href="link" 
+      target="_blank" 
+      class="nav-item nav-item-external"
+      :style="linkColor"
+    >
+      {{ text }}
+    </a>
   </div>
 </template>
 
 <script>
 export default {
   name: 'navItem',
-  props: ['text', 'link'],
+  props: ['text', 'link', 'color'],
+  computed: {
+    linkColor() {
+      if (this.color) {
+        // FIXME: obviously this does not work
+        return `
+        background: ${this.color};
+        @media (min-width: 800px){
+          background: transparent;
+          color: ${this.color};
+        }`
+      } else {
+        return ``
+      }
+    }
+  },
   methods: {
     isUrl(link) {
       let pattern = /^((http|https|ftp):\/\/)/
