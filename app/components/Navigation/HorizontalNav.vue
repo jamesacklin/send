40bfeb5031811a8fd9fd2140ed41575c8c9e2784
James Acklin <james.acklin@gmail.com>
2019-09-07 15:22:42 -0400

    nav changes

--- app/components/Navigation/HorizontalNav.vue
+++ app/components/Navigation/HorizontalNav.vue
@@ -40,7 +40,7 @@ export default {
 .horizontal-nav {
   flex-grow: 1;
   display: none;
-  @media (min-width: 800px) {
+  @media (min-width: 945px) {
     display: block;
   }
 }
@@ -49,33 +49,31 @@ export default {
   min-width: 23em;
   width: 0;
   margin: 0;
-  font-size: 70%
+  font-size: 66%
 }
 
 nav {
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
-  @media (min-width: 800px) {
+  @media (min-width: 945px) {
     flex-wrap: wrap;
-    padding-left: 1rem;
-    > div {
-      width: 33%;
-    }
-    .search-component {
-      align-self: flex-end;
-      width: 100% !important;
-      margin-top: 1em !important;
-    }
-  }
-  @media (min-width: 1150px) {
-    flex-wrap: nowrap;
     > div {
       width: auto;
     }
     .search-component {
       align-self: flex-end;
+      padding: 0 1em;
+      width: 100% !important;
+      margin-top: 1em !important;
+    }
+  }
+  @media (min-width: 1200px) {
+    flex-wrap: nowrap;
+    .search-component {
+      align-self: flex-end;
+      padding: 0;
       width: auto !important;
       margin-top: 0 !important;
     }
